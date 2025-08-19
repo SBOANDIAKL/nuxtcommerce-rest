e68381468b2f05bc2ce0b2d178ff016299f8943a
SBOANDIAKL <sonikbadal@gmail.com>
2025-08-19 21:26:49 +0545

    chore: format

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,17 +1,17 @@
-import 'dotenv/config';
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-import { NodePgDatabase } from 'drizzle-orm/node-postgres';
-import * as schema from '../db/schema';
+import type { NodePgDatabase } from 'drizzle-orm/node-postgres'
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { Pool } from 'pg'
+import * as schema from '../db/schema/index'
+import 'dotenv/config'
 
-let db_instance:NodePgDatabase<typeof schema> | null = null;
+let db_instance: NodePgDatabase<typeof schema> | null = null
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
-});
-export const db = () =>{
-    if(!db_instance){
-        db_instance = drizzle({ client: pool, schema, casing: 'snake_case' });
-    }
-    return db_instance;
+})
+export function db() {
+  if (!db_instance) {
+    db_instance = drizzle({ client: pool, schema, casing: 'snake_case' })
+  }
+  return db_instance
 }
